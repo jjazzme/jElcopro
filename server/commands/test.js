ec70cea701fe754603121189b590552c66110eaf
Valdimir <elcopro@gmail.com>
2019-11-02 21:31:22 +0300

    FSM in DocumentService

--- server/commands/test.js
+++ server/commands/test.js
@@ -1,21 +1,8 @@
+import InvoiceService from "../services/InvoiceService";
 
-import _ from 'lodash'
-import ExternalPriceService from "../services/ExternalPriceService";
-import PromelecService from "../services/PromelecService";
-import axios from 'axios';
-import PriceService from "../services/PriceService";
-import PartyService from "../services/PartyService";
-import CompanyService from "../services/CompanyService";
-import Entity from "../services/Entity";
-import ProductService from "../services/ProductService";
 
 module.exports.run = async (args) => {
-    const service = new ProductService();
-    const res = await service.find({name: 'max232cpe'});
-    //const service = await ExternalPriceService.forCompany('promelec');
-    //const res = await service.searchByName('LM2903MX');
-    //const service = new PromelecService();
-    //const res = await service.apiSearchByName('max232cpe');
-    //const res = await axios.get('https://ya.ru');
+    const service = new InvoiceService();
+    const res = await service.transition( 'toWork')
     console.log(res);
 };
--- server/services/DocumentService.js
+++ server/services/DocumentService.js
@@ -1,13 +1,60 @@
+import StateMachine from 'javascript-state-machine';
+import _ from 'lodash';
 import Entity from './Entity';
 
 export default class DocumentService extends Entity {
     /**
-     * Transitions
+     * Transitions used in children
+     * @type {Array}
+     * @private
      */
-    _transitions;
+    _transitions = [];
 
-    // eslint-disable-next-line no-useless-constructor
+    /**
+     *  Document instance
+     */
+    _instance;
+
     constructor(entity) {
         super(entity);
+        this._instance = null;
+    }
+
+    /**
+     *  Document status transition
+     * @param name
+     * @returns {Promise<boolean>}
+     */
+    async transition(name) {
+        if (this._instance) {
+            StateMachine.apply(this._instance, {
+                init: this._instance.status_id,
+                transitions: this._transitions,
+                methods: {
+                    [`onBefore${_.upperFirst(name)}`]: () => this[name](),
+                },
+            });
+            if (this._instance.can(name)) {
+                try {
+                    console.log(`Try ${name}`);
+                    await this._instance[name]();
+                    console.log(this._instance.state);
+                    this._instance.status = this._instance.state;
+                    return true;
+                } catch (e) {
+                    console.log('reject', e);
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Set Instance property
+     * @param instance
+     * @returns {Promise<void>}
+     */
+    async setInstance(instance) {
+        this._instance = await this.getInstance(instance);
     }
 }
