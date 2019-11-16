6d97084f48ebc499739d726c8b947eb4419a083b
jazz <me@xinet.ru>
2019-11-16 09:53:51 +0300

    dbseed3+

--- server/commands/dbSeed.js
+++ server/commands/dbSeed.js
@@ -1,20 +1,15 @@
 import fs from 'fs';
-
-require("@babel/core");
-require("@babel/polyfill");
-var config = require('../config/config.js');
-var models = require("../models");
+import models from "../models"
 
 const path = './commands/';
 
 module.exports.run = async () => {
-    var sequelize = models.sequelize;
+    const sequelize = models.sequelize;
     sequelize.sync({alter:true}).then(res => {
         console.log('Nice! Database looks fine')
     }).catch(err => {
         console.log(err, "Something went wrong with the Database Update!")
     });
-
     await fs.readdir(path, async (err, files) => {
         // eslint-disable-next-line global-require,import/no-dynamic-require
         const promises = files.filter((file) => file.indexOf('seed') === 0).map((file) => require(`./${file}`).run());
