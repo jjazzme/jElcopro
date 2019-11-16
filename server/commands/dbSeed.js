5054938b54fb3547ac690c91377ef4ab4fedf029
jazz <me@xinet.ru>
2019-11-16 10:00:04 +0300

    dbseed with sync

--- server/commands/dbSeed.js
+++ server/commands/dbSeed.js
@@ -3,17 +3,23 @@ import models from "../models"
 
 const path = './commands/';
 
-module.exports.run = async () => {
+module.exports.run = () => {
     const sequelize = models.sequelize;
     sequelize.sync({alter:true}).then(res => {
-        console.log('Nice! Database looks fine')
+        console.log('Nice! Database looks fine');
+
+        const seed = async() =>{
+            await fs.readdir(path, async (err, files) => {
+                // eslint-disable-next-line global-require,import/no-dynamic-require
+                const promises = files.filter((file) => file.indexOf('seed') === 0).map((file) => require(`./${file}`).run());
+                await Promise.all(promises);
+            });
+            console.log('All seed');
+
+        };
+
+        seed();
     }).catch(err => {
         console.log(err, "Something went wrong with the Database Update!")
     });
-    await fs.readdir(path, async (err, files) => {
-        // eslint-disable-next-line global-require,import/no-dynamic-require
-        const promises = files.filter((file) => file.indexOf('seed') === 0).map((file) => require(`./${file}`).run());
-        await Promise.all(promises);
-    });
-    console.log('All seed');
 };
