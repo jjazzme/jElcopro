0851606f69fb5d23085a862abf520d9ee88b0ef0
jazz <me@xinet.ru>
2019-11-16 09:46:57 +0300

    dbseed add dbsync

--- server/commands/dbSeed.js
+++ server/commands/dbSeed.js
@@ -1,8 +1,15 @@
 import fs from 'fs';
+import models from "../models"
 
 const path = './commands/';
 
 module.exports.run = async () => {
+    const sequelize = models.sequelize;
+    sequelize.sync({alter:true}).then(res => {
+        console.log('Nice! Database looks fine')
+    }).catch(err => {
+        console.log(err, "Something went wrong with the Database Update!")
+    });
     await fs.readdir(path, async (err, files) => {
         // eslint-disable-next-line global-require,import/no-dynamic-require
         const promises = files.filter((file) => file.indexOf('seed') === 0).map((file) => require(`./${file}`).run());
