2f73f9ac10b21b6b414f62bc9957a908824ab66b
jazz <me@xinet.ru>
2019-11-18 13:44:30 +0300

    первый вариант карты счёта+

---
+++ client/src/components/body/v1/headerComponents/orderInvoiceCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="s-h-invoice">
+    <b-link
+      :to="{name:'tables', params:{table:'Invoice'}}"
+      v-if="!value"
+    >
+      Добавить счёт
+    </b-link>
+    <div
+      v-else
+    >
+      <div class="h-i-close" @click="closeInvoice()">x</div>
+      <div class="h-i-topic">Счёт №{{invoice.number}} от {{Intl.DateTimeFormat('ru-RU').format(new Date(invoice.date))}}</div>
+      <div class="h-i-sum">{{invoice._sum}}₽</div>
+      <div class="h-i-lines">Строк: {{invoice.documentLines.length}} | Товаров: {{invoice._count}}</div>
+      <div class="h-i-buyer" :title="invoice.buyerable.party.name">{{invoice.buyerable.party.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "orderInvoiceCard",
+    props: {
+      value: null,
+    },
+    computed:{
+      docType(){return this.value.}
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
