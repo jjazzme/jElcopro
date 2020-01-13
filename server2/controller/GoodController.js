4844af8341e127072dd312085e32161793275bad
jazz <me@xinet.ru>
2020-01-13 15:22:05 +0300

    отображение айтемов +

--- server2/controller/GoodController.js
+++ server2/controller/GoodController.js
@@ -1,7 +1,11 @@
-import DocumentController from './DocumentController';
+import ApiController from './ApiController';
 
-export default class GoodController extends DocumentController {
+export default class GoodController extends ApiController {
     constructor(db) {
         super(db.models.Good);
+    }
+
+    async get(req) {
+        return this.Model.getInstance(parseInt(req.params.id, 0), this.scopes);
     }
 }
