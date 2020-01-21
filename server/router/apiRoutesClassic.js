ef1dbfae3827485ae453588ab2c37aa1f4bca78e
Valdimir <elcopro@gmail.com>
2020-01-21 17:49:33 +0300

    Client2 V0.3.1
    Vuetify

--- server/router/apiRoutesClassic.js
+++ server/router/apiRoutesClassic.js
@@ -1,12 +1,12 @@
 import ApiRouterClassic from './ApiRouterClassic';
-import ApiController from '../controller/ApiController';
+import ApiController from '../controller/ApiControllerClassic';
 
 export default function ApiRoutesClassic(services) {
     const apiRouter = new ApiRouterClassic(services.db);
 
     // apiRouter.middleware(services.auth.bearer);
 
-    apiRouter.resource('currency', new ApiController(services.db.models.Currency));
+    apiRouter.resource('producer', new ApiController(services.db.models.Producer));
 
     return apiRouter.router;
 }
